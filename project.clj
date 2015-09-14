c411ed55210f16c5093f09797ee3c1ccc213ef72
Brandon Adams <emidln@gmail.com>
2015-09-14 16:14:32 -0500

    New version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject curiosity.utils "0.6.0"
+(defproject curiosity.utils "0.7.0"
   :description "Misc clojure functions"
   :url "https://github.com/CuriosityApp/curiosity.utils"
   :license {:name "Eclipse Public License"
