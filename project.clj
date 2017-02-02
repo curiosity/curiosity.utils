653acc41a62e7bdef4426a5a8aa4955ab24312b8
Brandon Adams <emidln@gmail.com>
2017-02-02 13:15:53 -0600

    bump deps

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject curiosity.utils "0.9.0"
+(defproject curiosity.utils "0.9.1"
   :description "Misc clojure functions"
   :url "https://github.com/CuriosityApp/curiosity.utils"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins  [[codox "0.8.10"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [prismatic/schema "0.4.3"]
-                 [prismatic/plumbing "0.4.4"]
-                 [potemkin "0.4.1"]
-                 [com.taoensso/encore "2.4.2"]]
+                 [prismatic/schema "1.1.3"]
+                 [prismatic/plumbing "0.5.3"]
+                 [potemkin "0.4.3"]
+                 [com.taoensso/encore "2.88.2"]]
   :codox  {:src-dir-uri "http://github.com/curiosityapp/curiosity.utils/blob/master/"
            :src-linenum-anchor-prefix "L"})
 
